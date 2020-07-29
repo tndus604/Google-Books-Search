2f84fdfe12bdfcc0c0c11571b3287dc20772616c
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-28 20:52:26 -0400

    created navbar and jumbotron

---
+++ client/src/components/Jumbotron/jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+    <div className="jumbotron jumbotron-fluid">
+        <div className="container text-center">
+            <h1 className="display-4">Google Books Search</h1>
+            <p className="lead">Search for and Save Books of Interest</p>
+        </div>
+    </div>
+    )
+}
+
+export default Jumbotron;
