0f8ef29476bfe4ae0e8cd5c43e502d8c08d11dd0
Haley SuYoun Jeon <tndus0604@hotmail.com>
2020-07-29 22:32:54 -0400

    added components in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Main from '../src/components/Pages/main';
+import Saved from './components/Pages/saved';
 
 function App() {
   return (
     <div className="app">
-      <Main />
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/saved" component={Saved} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
